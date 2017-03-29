a58ce2014d4eda2f32ef2e98f29f1519b9e6fbcc
Ike Quigley <ikequigley@gmail.com>
2017-03-29 18:49:26 -0400

    Inclass Changes
    Edited Event class, and set BoffoRegister class to call on controller.

--- BoffoRegister/src/events/BoffoMessenger.java
+++ BoffoRegister/src/events/BoffoMessenger.java
@@ -8,11 +8,11 @@ public class BoffoMessenger {
     public static final int TRANSACTION_PANEL = 2;
     public static final int ADD_TRANSACTION = 3;
     private final int THIS_MESSAGE;
-       
+
     public BoffoMessenger(int message) {
-        this.THIS_MESSAGE = message; 
+        this.THIS_MESSAGE = message;
     }
-    
+
     public int getCode() {
         return this.THIS_MESSAGE;
     }
--- BoffoRegister/src/transaction/Transaction.java
+++ BoffoRegister/src/transaction/Transaction.java
@@ -1,16 +1,12 @@
-/**
- * Transaction Object
- */
-package modules;
+package transaction;
 
-import bofforegisterevents.RegisterEvent;
-import bofforegisterevents.RegisterMessage;
+import events.BoffoEvent;
 
 public class Transaction {
     public Transaction(){
            System.out.println("Transaction Module Loaded");
     }
-    
+
     /**
      * gets called when an item has been added to the ticket
     */
@@ -18,32 +14,34 @@ public class Transaction {
         //an item has been added to the ticker
         System.out.println("Item has been added to ticket.");
     }
-    
+
     /**
      * gets called when an item is removed from the ticket
      */
     public void removeItem(){
         System.out.println("Item has been removed from ticket.");
     }
-    
+
     /**
      * sends event to listener and awaits response
-     * @param event 
+     * @param event
      */
-    public void messageReceived(RegisterEvent event){
+    public void messageReceived(BoffoEvent event){
         switch(event.getMessage().getCode()){
+            /*
             case RegisterMessage.addProduct:
                 this.addItem();
                 break;
             case RegisterMessage.removeProduct:
                 this.removeItem();
                 break;
-            default: 
+            default:
                 System.out.println("Ignoring Message Event. "
                   + "Irrelevant to Transaction" + event.getMessage().getCode());
                 break;
+            */
         }
     }
-    
-    
+
+
 }
